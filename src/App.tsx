dec2dfb3ab9deefc238892c770b72f39815f69a3
jsimonassi <jsimonassi@id.uff.br>
2023-04-17 22:34:10 -0300

    Baseline - React App

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+        Aluno Presente!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+import useWindowDimensions from "./windowDimensions";
+
+export {
+	useWindowDimensions
+};
