86f63c37f03e69836b9aa4393dac479db2370b4c
jsimonassi <jsimonassi@id.uff.br>
2023-08-24 00:21:15 -0300

    start session manager

---
+++ src/hooks/session.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const useSession = () => {
+
+	// const [session, setSession] = useState<Session | null>(null);
+
+	// useEffect(() => {
+	// 	const session = getSession();
+	// 	setSession(session);
+	// }, []);
+
+	// return { session, setSession };
+	return null;
+};
+
+export default useSession;
+
+//TODO: Make Session interface and getSession function
